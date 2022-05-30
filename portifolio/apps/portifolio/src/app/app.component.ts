9d3c926d7df70bc28bc83f621476899380835f28
Giovanni Sarao Araki <giovanni.saraoaraki@gmail.com>
2022-05-30 17:12:19 -0300

    Feature - Loading screen

--- portifolio/apps/portifolio/src/app/app.component.ts
+++ portifolio/apps/portifolio/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'portifolio-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'portifolio';
+export class AppComponent implements OnInit {
+  loading = true;
+  ngOnInit(): void {
+    setTimeout(() => {this.loading = false},4000)
+  }
 }
